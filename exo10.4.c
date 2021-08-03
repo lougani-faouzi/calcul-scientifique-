956d9a389ae83592766c73cd16b6ad4bd9d691a2
lougani-faouzi <faouzi.lougani@hotmail.com>
2021-08-03 12:31:12 +0200

    exo10.4 overton

---
+++ exo10.4.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{       
+	double x=0.9999999;
+        printf("log de x=%.8f\n",log(x));
+        printf("x=%.8f\n",exp(log(x)));
+             
+return 0;
+
+}
+
+
