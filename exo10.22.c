0aa4b0956a071454a0fd4e7a22055a4a372b0726
lougani-faouzi <faouzi.lougani@hotmail.com>
2021-08-03 12:41:18 +0200

    exo10.22 overton

---
+++ exo10.22.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{       
+	
+        
+        float x=0.9999999f;
+        float y=sqrt(x);
+
+        printf("racine de x=%.8f\n",sqrt(x));
+        printf("x=%.8f\n",y*y);
+               
+
+return 0;
+
+}
